526432d9b78268024e57c2331af6b961e9c5286d
Moying <1309180832@qq.com>
2023-06-08 23:47:14 +0800

    项目初始化

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,17 @@
 <template>
-  
+  <div class="home">
+    这是首页
+  </div>
 </template>
 
 <script>
 export default {
+    name:'Home',
+    data() {
+        return {
+            
+        }
+    },
 
 }
 </script>
